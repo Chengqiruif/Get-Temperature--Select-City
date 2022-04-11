7c2e69a1cdc660548f47d4aa7c9c038fc4c8469f
Chengqiruif <102952060+Chengqiruif@users.noreply.github.com>
2022-04-11 08:40:30 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace wdj
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbltmp = new System.Windows.Forms.Label();
+            this.lblnum = new System.Windows.Forms.Label();
+            this.lblwt = new System.Windows.Forms.Label();
+            this.lblWind = new System.Windows.Forms.Label();
+            this.lblWindLv = new System.Windows.Forms.Label();
+            this.lbltxt3 = new System.Windows.Forms.Label();
+            this.lblzyx = new System.Windows.Forms.Label();
+            this.weatherDate = new System.Windows.Forms.DateTimePicker();
+            this.lbltxt2 = new System.Windows.Forms.Label();
+            this.lblwd = new System.Windows.Forms.Label();
+            this.lblto = new System.Windows.Forms.Label();
+            this.lblcd = new System.Windows.Forms.Label();
+            this.lblfrom = new System.Windows.Forms.Label();
+            this.lbltxt1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblcity = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbltmp
+            // 
+            resources.ApplyResources(this.lbltmp, "lbltmp");
+            this.lbltmp.Name = "lbltmp";
+            // 
+            // lblnum
+            // 
+            resources.ApplyResources(this.lblnum, "lblnum");
+            this.lblnum.Name = "lblnum";
+            // 
+            // lblwt
+            // 
+            resources.ApplyResources(this.lblwt, "lblwt");
+            this.lblwt.ForeColor = System.Drawing.Color.Black;
+            this.lblwt.Name = "lblwt";
+            // 
+            // lblWind
+            // 
+            resources.ApplyResources(this.lblWind, "lblWind");
+            this.lblWind.ForeColor = System.Drawing.Color.Turquoise;
+            this.lblWind.Name = "lblWind";
+            // 
+            // lblWindLv
+            // 
+            resources.ApplyResources(this.lblWindLv, "lblWindLv");
+            this.lblWindLv.ForeColor = System.Drawing.Color.Black;
+            this.lblWindLv.Name = "lblWindLv";
+            // 
+            // lbltxt3
+            // 
+            resources.ApplyResources(this.lbltxt3, "lbltxt3");
+            this.lbltxt3.ForeColor = System.Drawing.Color.DarkSlateBlue;
+            this.lbltxt3.Name = "lbltxt3";
+            // 
+            // lblzyx
+            // 
+            resources.ApplyResources(this.lblzyx, "lblzyx");
+            this.lblzyx.ForeColor = System.Drawing.Color.Black;
+            this.lblzyx.Name = "lblzyx";
+            // 
+            // weatherDate
+            // 
+            this.weatherDate.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            resources.ApplyResources(this.weatherDate, "weatherDate");
+            this.weatherDate.Name = "weatherDate";
+            this.weatherDate.ValueChanged += new System.EventHandler(this.weatherDate_ValueChanged);
+            // 
+            // lbltxt2
+            // 
+            resources.ApplyResources(this.lbltxt2, "lbltxt2");
+            this.lbltxt2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.lbltxt2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lbltxt2.Name = "lbltxt2";
+            // 
+            // lblwd
+            // 
+            resources.ApplyResources(this.lblwd, "lblwd");
+            this.lblwd.ForeColor = System.Drawing.Color.Black;
+            this.lblwd.Name = "lblwd";
+            // 
+            // lblto
+            // 
+            resources.ApplyResources(this.lblto, "lblto");
+            this.lblto.Name = "lblto";
+            // 
+            // lblcd
+            // 
+            resources.ApplyResources(this.lblcd, "lblcd");
+            this.lblcd.Name = "lblcd";
+            // 
+            // lblfrom
+            // 
+            resources.ApplyResources(this.lblfrom, "lblfrom");
+            this.lblfrom.Name = "lblfrom";
+            // 
+            // lbltxt1
+            // 
+            resources.ApplyResources(this.lbltxt1, "lbltxt1");
+            this.lbltxt1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lbltxt1.Name = "lbltxt1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblcity);
+            this.groupBox1.Controls.Add(this.lbltmp);
+            this.groupBox1.Controls.Add(this.lblnum);
+            this.groupBox1.Controls.Add(this.lblwt);
+            this.groupBox1.Controls.Add(this.lblWind);
+            this.groupBox1.Controls.Add(this.lblWindLv);
+            this.groupBox1.Controls.Add(this.lbltxt3);
+            this.groupBox1.Controls.Add(this.lblzyx);
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.ForeColor = System.Drawing.Color.BurlyWood;
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // lblcity
+            // 
+            resources.ApplyResources(this.lblcity, "lblcity");
+            this.lblcity.ForeColor = System.Drawing.Color.Tan;
+            this.lblcity.Name = "lblcity";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.weatherDate);
+            this.groupBox2.Controls.Add(this.lbltxt2);
+            this.groupBox2.Controls.Add(this.lblcd);
+            this.groupBox2.Controls.Add(this.lbltxt1);
+            this.groupBox2.Controls.Add(this.lblto);
+            this.groupBox2.Controls.Add(this.lblfrom);
+            this.groupBox2.Controls.Add(this.lblwd);
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.ForeColor = System.Drawing.Color.SkyBlue;
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.SandyBrown;
+            this.label1.Name = "label1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.ForeColor = System.Drawing.Color.LightCoral;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.toolTip1.SetToolTip(this.button1, resources.GetString("button1.ToolTip"));
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbltmp;
+        private System.Windows.Forms.Label lblnum;
+        private System.Windows.Forms.Label lblwt;
+        private System.Windows.Forms.Label lblWind;
+        private System.Windows.Forms.Label lblWindLv;
+        private System.Windows.Forms.Label lbltxt3;
+        private System.Windows.Forms.Label lblzyx;
+        private System.Windows.Forms.DateTimePicker weatherDate;
+        private System.Windows.Forms.Label lbltxt2;
+        private System.Windows.Forms.Label lblwd;
+        private System.Windows.Forms.Label lblto;
+        private System.Windows.Forms.Label lblcd;
+        private System.Windows.Forms.Label lblfrom;
+        private System.Windows.Forms.Label lbltxt1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblcity;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,378 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace wdj
+{
+    public partial class Form1 : Form
+    {
+        bool change = false;
+        string english = "";
+        WebClient wc = new WebClient();
+        Thread th;
+
+        ~Form1()
+        {
+            wc.Dispose();
+            GC.Collect();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string GetIP()
+        {
+            string pageHtml = string.Empty;
+            try
+            {
+                using (WebClient MyWebClient = new WebClient())
+                {
+                    Encoding encode = Encoding.GetEncoding("gbk");
+                    MyWebClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36");
+                    MyWebClient.Credentials = CredentialCache.DefaultCredentials;//获取或设置用于向Internet资源的请求进行身份验证的网络凭据
+                    Byte[] pageData = MyWebClient.DownloadData("http://www.net.cn/static/customercare/yourip.asp"); //从指定网站下载数据
+                    pageHtml = encode.GetString(pageData);
+                }
+            }
+            catch { }
+            string reg = @"(?:(?:(25[0-5])|(2[0-4]\d)|((1\d{2})|([1-9]?\d)))\.){3}(?:(25[0-5])|(2[0-4]\d)|((1\d{2})|([1-9]?\d)))";
+            string ip = "";
+            Match m = Regex.Match(pageHtml, reg);
+            if (m.Success)
+            {
+                ip = m.Value;
+            }
+            return ip;
+        }
+        private Color GetColor(double tmp)
+        {
+            Color color = new Color();
+            if (comboBox1.SelectedIndex == 1)
+            {
+                tmp = (tmp-32)*5/9;
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                tmp -= 273.15;
+            }
+            else
+            {
+                tmp = tmp;
+            }
+            r:try
+            {
+                if (tmp < 15 && tmp > -51)
+                {
+                    color = Color.FromArgb(0, 0, (int)((65 - (tmp + 50)) * 3));
+                }
+                else if (tmp < 26 && tmp > 14)
+                {
+                    color = Color.FromArgb(0, (int)((11 - (tmp - 14)) * 11) + 127, 0);
+                }
+                else
+                {
+                    color = Color.FromArgb((int)(255 - ((25 - (tmp - 14)) * 5)), 0, 0);
+                }
+            }
+            catch 
+            {
+                if (tmp < 123)
+                    tmp = (tmp - 32) * 5 / 9;
+                else
+                    tmp -= 273.15;
+                goto r;
+            }
+            GC.Collect();
+            return color;
+        }
+        private void GetTemp()
+        {
+            while(true)
+            {
+                if (!change)
+                    goto e;
+                string html = "";
+                r:try
+                {
+                    html = Encoding.UTF8.GetString(wc.DownloadData("https://www.tianqi.com/" + english + "/"));
+                }
+                catch
+                {
+                    goto r;
+                }
+                string[] lines = html.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                int index = 152;
+                string strC = lines[index].Substring(18);
+                double c = double.Parse(strC.Substring(0, strC.IndexOf('<')));
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    c = c * 9 / 5 + 32;
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    c += 273.15;
+                }
+                else
+                {
+                    c = c;
+                }
+                lbltmp.ForeColor = GetColor(c);
+                lblnum.ForeColor = GetColor(c);
+                lblnum.Text = c.ToString();
+                index++;
+                strC = lines[index].Substring(9);
+                strC = strC.Substring(0, strC.IndexOf('<'));
+                switch (strC)
+                {
+                    case "晴":
+                        lblwt.ForeColor = Color.Orange;
+                        break;
+                    case "多云":
+                        lblwt.ForeColor = Color.LightSkyBlue;
+                        break;
+                    case "阴":
+                        lblwt.ForeColor = Color.Gray;
+                        break;
+                    case "雨夹雪":
+                        lblwt.ForeColor = Color.Blue;
+                        break;
+                    default:
+                        lblwt.ForeColor = Color.Black;
+                        break;
+                }
+                lblwt.Text = strC;
+                string temp2;
+                index += 2;
+                strC = lines[index].Substring(24);
+                strC = strC.Substring(13);
+                temp2 = strC.Substring(0, strC.IndexOf('<'));
+                lblWind.Text = temp2.Substring(0, temp2.IndexOf(' '));
+                lblWindLv.Text = temp2.Substring(temp2.IndexOf(' ') + 1);
+                strC = strC.Substring(strC.IndexOf('：') + 1);
+                lblzyx.Text = strC.Substring(0, strC.IndexOf('<'));
+                if (lblzyx.Text.Contains("强"))
+                    lblzyx.ForeColor = Color.BlueViolet;
+                else
+                    lblzyx.ForeColor = Color.Blue;
+                e: Thread.Sleep(100);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            weatherDate.Value = DateTime.Now;
+            weatherDate.MinDate = DateTime.Now;
+            weatherDate.MaxDate = DateTime.Now.AddDays(39);
+            Control.CheckForIllegalCrossThreadCalls = false;
+            th = new Thread(GetTemp) { IsBackground = true };
+            th.Start();
+            try
+            {
+                throw new Exception();
+                string http = "https://www.ip138.com/iplookup.asp?ip=" + GetIP() + "&action=2";
+                byte[] buffer = wc.DownloadData(http);
+                string show = Encoding.Default.GetString(buffer);
+                string[] temp = show.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                string city = temp[25].Substring(33, temp[25].IndexOf("市") - 33);
+                lblcity.Text = city;
+                english = PinYin.Trim(PinYin.ToLower(PinYin.RemoveAttributeChars(PinYin.ToPinYin(city))));
+            }
+            catch
+            {
+                if (new ErrorForm().ShowDialog() == DialogResult.OK)
+                {
+                    english = PinYin.Trim(PinYin.ToLower(PinYin.RemoveAttributeChars(PinYin.ToPinYin(Share.city))));
+                    lblcity.Text = Share.city+"市";
+                }
+            }
+            string html = Encoding.UTF8.GetString(wc.DownloadData("https://www.tianqi.com/" + english + "/"));
+            string[] lines = html.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            int index = 152;
+            string strC = lines[index].Substring(18);
+            double c = double.Parse(strC.Substring(0, strC.IndexOf('<')));
+            lbltmp.ForeColor = GetColor(c);
+            lblnum.ForeColor = GetColor(c);
+            lblnum.Text = c.ToString();
+            index++;
+            strC = lines[index].Substring(9);
+            strC = strC.Substring(0, strC.IndexOf('<'));
+            switch (strC)
+            {
+                case "晴":
+                    lblwd.ForeColor = Color.Orange;
+                    break;
+                case "多云":
+                    lblwd.ForeColor = Color.LightSkyBlue;
+                    break;
+                case "阴":
+                    lblwd.ForeColor = Color.Gray;
+                    break;
+                case "雨夹雪":
+                    lblwt.ForeColor = Color.Blue;
+                    break;
+                default:
+                    lblwd.ForeColor = Color.Black;
+                    break;
+            }
+            lblwt.Text = strC;
+            string temp2;
+            index += 2;
+            strC = lines[index].Substring(24);
+            strC = strC.Substring(13);
+            temp2 = strC.Substring(0, strC.IndexOf('<'));
+            lblWind.Text = temp2.Substring(0,temp2.IndexOf(' '));
+            lblWindLv.Text = temp2.Substring(temp2.IndexOf(' ')+1);
+            strC = strC.Substring(strC.IndexOf('：') + 1);
+            lblzyx.Text = strC.Substring(0,strC.IndexOf('<'));
+            if(lblzyx.Text.Contains("强"))
+                lblzyx.ForeColor = Color.BlueViolet;
+            else
+                lblzyx.ForeColor = Color.Blue;
+            switch(lblWindLv.Text[0])
+            {
+                case '0':
+                    lblWindLv.ForeColor = Color.Gray;
+                    break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                    lblWindLv.ForeColor = Color.DeepSkyBlue;
+                    break;
+                default:
+                    lblWindLv.ForeColor = Color.Red;
+                    break;
+            }
+            try
+            {
+                html = Encoding.UTF8.GetString(wc.DownloadData("https://www.tianqi.com/" + english + "/40"));
+                lines = html.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                strC = lines[0 * 11 + 184].Substring(21);
+                lblwd.Text = strC.Substring(0, strC.IndexOf('<'));
+                switch (lblwd.Text)
+                {
+                    case "晴":
+                        lblwt.ForeColor = Color.Orange;
+                        break;
+                    case "多云":
+                        lblwt.ForeColor = Color.LightSkyBlue;
+                        break;
+                    case "阴":
+                        lblwt.ForeColor = Color.Gray;
+                        break;
+                    case "雨夹雪":
+                        lblwt.ForeColor = Color.Blue;
+                        break;
+                    default:
+                        lblwt.ForeColor = Color.Black;
+                        break;
+                }
+                strC = lines[0 * 11 + 185].Substring(27);
+                lblfrom.Text = strC.Substring(0, strC.IndexOf('<'));
+                lblfrom.ForeColor = GetColor(double.Parse(strC.Substring(0, strC.IndexOf('<'))));
+                strC = strC.Substring(16);
+                lblto.Text = strC.Substring(0, strC.IndexOf('<'));
+                lblto.ForeColor = GetColor(double.Parse(strC.Substring(0, strC.IndexOf('<'))));
+                lblcd.ForeColor = GetColor(double.Parse(strC.Substring(0, strC.IndexOf('<'))));
+            }
+            catch { }
+            change = true;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void weatherDate_ValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!change)
+                    return;
+                string html = Encoding.UTF8.GetString(wc.DownloadData("https://www.tianqi.com/" + english + "/40"));
+                string[] lines = html.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+                string strC = lines[(weatherDate.Value - DateTime.Now).Days * 11 + 184].Substring(21);
+                lblwd.Text = strC.Substring(0, strC.IndexOf('<'));
+                switch (strC.Substring(0, strC.IndexOf('<')))
+                {
+                    case "晴":
+                        lblwd.ForeColor = Color.Orange;
+                        break;
+                    case "多云":
+                        lblwd.ForeColor = Color.LightSkyBlue;
+                        break;
+                    case "阴":
+                        lblwd.ForeColor = Color.Gray;
+                        break;
+                    case "雨夹雪":
+                        lblwd.ForeColor = Color.Blue;
+                        break;
+                    default:
+                        lblwd.ForeColor = Color.Black;
+                        break;
+                }
+                strC = lines[(weatherDate.Value - DateTime.Now).Days * 11 + 185].Substring(27);
+                strC = strC.Substring(0, strC.IndexOf('<'));
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    strC = (double.Parse(strC) * 9 / 5 + 32).ToString();
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    strC = (double.Parse(strC) + 273.15).ToString();
+                }
+                else
+                {
+                    strC = strC;
+                }
+                lblfrom.Text = strC;
+                lblfrom.ForeColor = GetColor(double.Parse(strC));
+                strC = lines[(weatherDate.Value - DateTime.Now).Days * 11 + 185].Substring(27);
+                strC = strC.Substring(16);
+                strC = strC.Substring(0, strC.IndexOf('<'));
+                if (comboBox1.SelectedIndex == 1)
+                {
+                    strC = (double.Parse(strC) * 9 / 5 + 32).ToString();
+                }
+                else if (comboBox1.SelectedIndex == 2)
+                {
+                    strC = (double.Parse(strC) + 273.15).ToString();
+                }
+                else
+                {
+                    strC = strC;
+                }
+                lblto.Text = strC;
+                lblto.ForeColor = GetColor(double.Parse(strC));
+                lblcd.ForeColor = GetColor(double.Parse(strC));
+            }
+            catch { }
+        }
+        private void lblcity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!change)
+                return;
+            english = PinYin.Trim(PinYin.ToLower(PinYin.RemoveAttributeChars(PinYin.ToPinYin(lblcity.Text))));
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lblcd.Text = comboBox1.Text;
+            lbltmp.Text = comboBox1.Text;
+            weatherDate_ValueChanged(sender, e);
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            new Thread(Program.Main) { IsBackground = false }.Start();
+            this.Close();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            th.Abort();
+        }
+    }
+}
